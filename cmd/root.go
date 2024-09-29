16cef398218b277b6028e2e9736292d0f0b6028b
kzankpe <78028172+kzankpe@users.noreply.github.com>
2024-09-29 18:07:59 +0000

    feat(#2): add initial template for cli

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,36 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "task-cli",
+	Short: "A CLI app to track your tasks and manage your to-do list",
+	Long:  "A CLI app to track your tasks and manage your to-do list - Add, Update, List, and Delete tasks.",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Oops. An error while executing Zero '%s'\n", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-sample-cli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
